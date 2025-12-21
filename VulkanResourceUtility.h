39300632a5752b4aed15098257cdb74868edb050
Atori708 <atori708@gmail.com>
2025-12-21 23:02:20 +0900

    追加漏れ

--- VulkanResourceUtility.h
+++ VulkanResourceUtility.h
@@ -9,6 +9,8 @@
 /// </summary>
 extern std::vector<char> readFile(const std::string& filename);
 
+extern std::vector<uint32_t> readFileAsUint32(const std::string& filename) ;
+
 extern uint32_t findMemoryType(const VkPhysicalDevice physicalDevice, const uint32_t typeFilter, const VkMemoryPropertyFlags properties);
 
 extern bool hasStencilFormat(VkFormat format);
